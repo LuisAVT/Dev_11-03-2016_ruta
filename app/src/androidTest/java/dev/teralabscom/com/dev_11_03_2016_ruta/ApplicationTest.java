7882b258588aea6e8c8a42e9a0e3f6785c9ed70e
damianlongoria <damianpedro@outlook.es>
2016-03-11 00:58:55 -0600

    contenido ruta a tiempo

---
+++ app/src/androidTest/java/dev/teralabscom/com/dev_11_03_2016_ruta/ApplicationTest.java
@@ -0,0 +1,13 @@
+package dev.teralabscom.com.dev_11_03_2016_ruta;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/dev/teralabscom/com/dev_11_03_2016_ruta/MainActivity.java
@@ -0,0 +1,13 @@
+package dev.teralabscom.com.dev_11_03_2016_ruta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
