2c149a1ef1bb805c11469ec3641d12220ab44c43
LuisAVT <dimebag_cfh_@hotmail.com>
2016-03-11 22:20:51 -0600

    cambio de favsactivity a report

---
+++ app/src/main/java/dev/teralabscom/com/dev_11_03_2016_ruta/Report.java
@@ -0,0 +1,16 @@
+package dev.teralabscom.com.dev_11_03_2016_ruta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Report extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report);
+
+    }
+
+
+}
